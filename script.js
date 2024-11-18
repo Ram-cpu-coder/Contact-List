62d7479910f6c151fecaa45004e4a3385c37bd5b
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-18 15:18:21 +1100

    for today done

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+// ==============================================
+const slider = document.getElementById("slider");
+
+slider.addEventListener("change", (e) => {
+    const { value } = e.target;
+
+    if (value < 70) {
+        slider.value = 0;
+
+    } else {
+        displayAppScreen();
+    }
+})
+
+// ==============================================
+const displayScreen = (screenName)=>{
+    const screens = document.getElementsByClassName("screen");
+
+    for(screen of screens) {
+        screen.style.display = "none";
+    }
+    const mainScreen = document.getElementById(screenName);
+
+    mainScreen.style.display = "block";
+}
+
+// ==============================================
+const displayAppScreen = ()=>{
+    
+    // appScreen.classList.remove("hidden");
+
+    displayScreen("appScreen");
+}
+
+// ==============================================
