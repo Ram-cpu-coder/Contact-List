a1e21bac25d5a6ea811e443dc48c0cd0dbd4b9a1
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-19 13:51:20 +1100

    date and time in lock screen

--- script.js
+++ script.js
@@ -2,6 +2,96 @@
 //================================================
 //apiUrl to get the random user data
 const apiUrl = "https://randomuser.me/api?results=100";
+
+// =====================================================
+
+const dayElm = document.getElementById("day");
+const timeElm = document.getElementById("time");
+const today = new Date();
+let day = today.getDay();
+let month = today.getMonth();
+const date = today.getDate();
+const hour = today.getHours() % 12;
+const minute = today.getMinutes();
+
+switch (day) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+
+    default:
+        day = "";
+        break;
+}
+switch (month) {
+    case 0:
+        month = "January";
+        break;
+    case 1:
+        month = "February";
+        break;
+
+    case 2:
+        month = "March";
+        break;
+    case 3:
+        month = "April";
+        break;
+
+    case 4:
+        month = "May";
+        break;
+    case 5:
+        month = "June";
+        break;
+
+    case 6:
+        month = "July";
+        break;
+    case 7:
+        month = "August";
+        break;
+
+    case 8:
+        month = "September";
+        break;
+    case 9:
+        month = "October";
+        break;
+
+    case 10:
+        month = "November";
+        break;
+    case 11:
+        month = "December";
+        break;
+
+    default:
+        month = "";
+        break;
+}
+dayElm.innerText = `${day}, ${date} ${month}`;
+timeElm.innerText = `${hour}:${minute}`;
+// =====================================================
 // initial contactList array
 let contactList = [{
     gender: "male",
